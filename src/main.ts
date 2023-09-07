78cbd190e2f73db2f449b95075dc66915eaf83a9
Hugh Nguyen <hhnguyen255@gmail.com>
2023-09-07 17:47:51 +0700

    setup prettier & initial deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import "./style.scss";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+import './style.scss';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /* html */ `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -21,4 +21,4 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
   </div>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
