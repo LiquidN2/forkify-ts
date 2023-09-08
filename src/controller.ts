88e1397c53692392c798f871697d2340eed6883d
Hugh Nguyen <hhnguyen255@gmail.com>
2023-09-08 16:19:39 +0700

    render recipe and spinner

--- src/controller.ts
+++ src/controller.ts
@@ -1,26 +1,172 @@
-// const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector('.recipe') as HTMLDivElement;
 
-// const timeout = function (s: number) {
-//   return new Promise(function (_, reject) {
-//     setTimeout(function () {
-//       reject(new Error(`Request took too long! Timeout after ${s} second`));
-//     }, s * 1000);
-//   });
-// };
+const timeout = function (s: number) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
 
-// const API_KEY: string = import.meta.env.VITE_FORKIFY_API_KEY;
+const API_KEY: string = import.meta.env.VITE_FORKIFY_API_KEY;
 
-// const showRecipe = async () => {
-//   try {
-//     const res = await fetch(
-//       `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=${API_KEY}`
-//     );
-//     const data = await res.json();
+interface Ingredient {
+  description: string;
+  quantity: number | null;
+  unit: string;
+}
 
-//     console.log(res, data);
-//   } catch (error: any) {
-//     alert(error);
-//   }
-// };
+interface Recipe {
+  id: string;
+  title: string;
+  publisher: string;
+  source_url: string;
+  image_url: string;
+  servings: number;
+  cooking_time: number;
+  ingredients: Ingredient[];
+}
 
-// shoRecipe();
+// type SearchResult = Partial<Recipe>;
+
+const renderSpinner = (parentElement: HTMLElement): void => {
+  const spinnerMarkup = /* html */ `
+    <div class="spinner">
+      <svg>
+        <use href="src/img/icons.svg#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+
+  parentElement.innerHTML = '';
+  parentElement.insertAdjacentHTML('afterbegin', spinnerMarkup);
+};
+
+const showRecipe = async (recipeId: string) => {
+  try {
+    renderSpinner(recipeContainer);
+
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    recipeContainer.innerHTML = '';
+    const recipe: Recipe = data.data.recipe;
+    const {
+      title,
+      publisher,
+      source_url,
+      image_url,
+      servings,
+      cooking_time,
+      ingredients,
+    } = recipe;
+
+    const markup = /*html*/ `
+      <figure class="recipe__fig">
+        <img src="${image_url}" alt="${title}" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${title}</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="src/img/icons.svg#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${cooking_time}</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="src/img/icons.svg#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${servings}</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="src/img/icons.svg#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="src/img/icons.svg#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated">
+          <svg>
+            <use href="src/img/icons.svg#icon-user"></use>
+          </svg>
+        </div>
+
+        <button class="btn--round">
+          <svg class="">
+            <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${ingredients
+            .map(ingredient => {
+              return /* html */ `
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+              ${
+                ingredient.quantity
+                  ? /*html*/ `<div class="recipe__quantity">${ingredient.quantity}</div>`
+                  : ''
+              }
+              <div class="recipe__description">
+                <span class="recipe__unit">${ingredient.unit}</span>
+                ${ingredient.description}
+              </div>
+            </li>
+            `;
+            })
+            .join('')}
+        </ul>
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${publisher}</span>. Please check out
+          directions at their website.
+        </p>
+        <a
+          class="btn--small recipe__btn"
+          href="${source_url}"
+          target="_blank"
+        >
+          <span>Directions</span>
+          <svg class="search__icon">
+            <use href="src/img/icons.svg#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </div>
+    `;
+
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+  } catch (error: any) {
+    alert(error);
+  }
+};
+
+showRecipe('5ed6604591c37cdc054bcd09');
